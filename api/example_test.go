b86814deb3236c8e0b4c265bca71cbe163e5f24e
Jesse Lucas <jesse@forestgiant.com>
2016-12-10 10:56:09 -0500

    Creating new example_test

--- api/example_test.go
+++ api/example_test.go
@@ -1,33 +1,55 @@
 package api
 
+import (
+	"fmt"
+	"log"
+
+	"gitlab.fg/go/stela"
+)
+
 func ExampleStelaApi() {
-	// // Create stela client
-	// stelaIPAddress := stela.DefaultStelaHTTPAddress // The default is localhost
-	// client, err := NewClient(stelaIPAddress)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Create stela client
+	client, err := NewClient(stela.DefaultStelaAddress, "../testdata/ca.pem") // The default is localhost
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
 
-	// // Create a service
-	// service := new(stela.Service)
-	// service.Name = "test.service.fg"
-	// service.Port = 8001
+	serviceName := "test.service.fg"
 
-	// // Now register with stela
-	// client.RegisterService(service)
+	// Create a service
+	service := &stela.Service{
+		Name: serviceName,
+		Port: 8001,
+	}
 
-	// // Discover all endpoints registered with that name
-	// // Typically done from another service
-	// services, err := client.Discover("test.service.fg")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Subscribe to that serviceName to be notified if anyone else registers a service
+	client.Subscribe(serviceName, func(s *stela.Service) {
+		switch s.Action {
+		case stela.RegisterAction:
+			fmt.Println("a new test.service.fg was found!", s)
+		case stela.DeregisterAction:
+			fmt.Println("a service was removed", s)
+		}
+	})
 
-	// fmt.Println("Found services:", services)
+	// Now register with stela
+	if err := client.RegisterService(service); err != nil {
+		log.Fatal(err)
+	}
 
-	// // When finished deregister
-	// err = client.DeregisterService(service)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Discover all endpoints registered with that name
+	// Typically done from another service
+	services, err := client.Discover(serviceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Found services:", services)
+
+	// When finished deregister
+	err = client.DeregisterService(service)
+	if err != nil {
+		log.Fatal(err)
+	}
 }

c4ce0f9ae5f6f899eba9cec041269298ae8f4203
Jesse Lucas <jesse@forestgiant.com>
2017-01-13 14:24:53 -0600

    Creating valid method for watcher and separating updateRegistry from watch method

--- cmd/stelawd/watcher.go
+++ cmd/stelawd/watcher.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"strconv"
@@ -18,31 +19,60 @@ type watcher struct {
 	stelaClient *api.Client
 }
 
-func (w *watcher) watch() {
+func (w *watcher) valid() bool {
+	// Make sure interval is positive
+	if w.interval < 0 || w.interval == 0 {
+		return false
+	}
+	if w.service == nil {
+		return false
+	}
+	if w.stelaClient == nil {
+		return false
+	}
+	if w.service.Name == "" {
+		return false
+	}
+	if w.service.Address == "" {
+		return false
+	}
+
+	return true
+}
+
+func (w *watcher) watch() error {
+	if !w.valid() {
+		return errors.New("watcher is invalid")
+	}
+
 	// Based on the interval in the config check net.Listen to see if the address/port is taken.
 	w.ticker = time.NewTicker(w.interval)
 
-	updateRegistry := func() {
-		registerCtx, cancelRegister := context.WithTimeout(context.Background(), 50*time.Millisecond)
-		defer cancelRegister()
-
-		// Verify service is running
-		conn, err := net.Dial("tcp", net.JoinHostPort(w.service.Address, strconv.Itoa(int(w.service.Port))))
-		// If there was an error that means the connection didn't happen and we need to deregister the service
-		if err != nil {
-			w.stelaClient.DeregisterService(registerCtx, w.service)
-			return
-		}
-		conn.Close()
-		w.stelaClient.RegisterService(registerCtx, w.service)
-	}
-
 	go func() {
 		for range w.ticker.C {
-			updateRegistry()
+			w.updateRegistry()
 		}
 	}()
 
+	return nil
+}
+
+// updateRegistry registers or deregisters a service with the stela client based on net.Dial
+// if it registers it returns true, if it deregisters it returns false
+func (w *watcher) updateRegistry() bool {
+	registerCtx, cancelRegister := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancelRegister()
+
+	// Verify service is running
+	conn, err := net.Dial("tcp", net.JoinHostPort(w.service.Address, strconv.Itoa(int(w.service.Port))))
+	// If there was an error that means the connection didn't happen and we need to deregister the service
+	if err != nil {
+		w.stelaClient.DeregisterService(registerCtx, w.service)
+		return false
+	}
+	conn.Close()
+	w.stelaClient.RegisterService(registerCtx, w.service)
+	return true
 }
 
 func (w *watcher) String() string {
@@ -50,5 +80,8 @@ func (w *watcher) String() string {
 }
 
 func (w *watcher) stop() {
+	if w.ticker == nil {
+		return
+	}
 	w.ticker.Stop()
 }

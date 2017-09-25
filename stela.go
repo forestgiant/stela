dfd656f2e8596a0e45be4f7d93546424400a080e
Jesse Lucas <jesse@forestgiant.com>
2017-09-25 11:35:14 -0400

    Removing vendor folder and adding dep for dependency management https://github.com/golang/dep

--- stela.go
+++ stela.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// Version of stela
-	Version = "0.11.3"
+	Version = "0.11.4"
 
 	// DefaultStelaAddress by default stela assumes there is a local instance running
 	DefaultStelaAddress = "127.0.0.1:31000"
